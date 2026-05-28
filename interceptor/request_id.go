cdc4526a4b78eb79a47ccd61de85ab559809e80c
Никита Алексеев <viktorifoks2004@mail.ru>
2026-05-29 02:49:36 +0300

    add: outgoing request

--- interceptor/request_id.go
+++ interceptor/request_id.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type ctxKey string
@@ -24,4 +25,37 @@ func RequestID() grpc.UnaryServerInterceptor {
 
 		return handler(ctx, req)
 	}
+}
+
+func XRequestIDClient() grpc.UnaryClientInterceptor {
+
+    return func(
+        ctx context.Context,
+        method string,
+        req interface{},
+        reply interface{},
+        cc *grpc.ClientConn,
+        invoker grpc.UnaryInvoker,
+        opts ...grpc.CallOption,
+    ) error {
+
+        md, ok := metadata.FromIncomingContext(ctx)
+
+        if ok {
+
+            ctx = metadata.NewOutgoingContext(
+                ctx,
+                md,
+            )
+        }
+
+        return invoker(
+            ctx,
+            method,
+            req,
+            reply,
+            cc,
+            opts...,
+        )
+    }
 }
